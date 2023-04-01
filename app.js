18f2e58de998a5ba9ffdbb9d19112eabf8233c5d
Sean <34967@ma-web.nl>
2023-04-01 19:33:52 +0200

    mini changes clock

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ function displayTime(){
     var hour = d.getHours();
     var min = d.getMinutes();
     var sec = d.getSeconds();
+
+    //? is een korte versie van if-else
+    //dus als 'hour' waar is dus (hour < 10) zet een 0 ervoor
+
+    hour = (hour < 10) ? "0" + hour : hour;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+    
     document.getElementById("js--clock").innerHTML = hour + ":" + min + ":" + sec;
 }
 
