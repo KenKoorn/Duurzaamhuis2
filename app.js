f9d0ceb5a034b18ca934e70d9883c617361b159a
Sean <34967@ma-web.nl>
2023-04-07 22:26:55 +0200

    new changes

--- app.js
+++ app.js
@@ -19,3 +19,32 @@ setInterval(displayTime , 1000);
 
 let date = new Date();
 console.log(date.getTime());
+
+
+const labels = [
+    "Week 1",
+    "Week 2",
+    "Week 3",
+    "Week 4",
+    "Week 5"
+];
+
+const data = {
+    labels: labels,
+    datasets:[
+        {
+            label: "Stroom-verbruik in het maand Januari",
+            data: [11.06 ,10.96, 10.89, 10.90, 10.75, 10.87],
+            backgroundColor: ['#8DCBE6', '#fff', '#CBE4DE', '#2C3333', '#ddd' ]
+        }
+    ]
+}
+
+
+const config = {
+    type: 'line',
+    data: data,
+}
+
+new Chart(document.getElementById("js--chart--1"), config);
+
